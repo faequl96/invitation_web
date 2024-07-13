ba034a0eb5db6ed8f3e7ebefd18f0b2489fa5466
faiq <faiq@voltras.co.id>
2024-07-13 09:24:21 +0700

    test

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,23 +1,30 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const loading = document.createElement('div');
-loading.style.width = '100vw';
-loading.style.height = '100vw';
-loading.style.alignContent = "center";
-loading.style.alignItems = "center";
-const text = document.createElement('p');
-document.body.appendChild(loading);
-loading.appendChild(text);
+const loadingContainer = document.createElement('div');
+loadingContainer.innerHTML = `
+  <div class="bodyload">
+    <div id="splash">
+      <div class="loader"></div>
+    </div>
+    <div id="splashtext" style="color: #3BAF29;">
+      <h3 id="loadText">mengunduh konten</h3>
+    </div>
+  </div>
+`;
 
-loading.textContent = "mengunduh konten";
+document.body.appendChild(loadingContainer);
+let loadText = document.getElementById("loadText");
+// loadText.textContent = "mengunduh konten";
+
+// loading.textContent = "mengunduh konten";
 // loading.setAttribute("align", "center");
 _flutter.loader.load({
   onEntrypointLoaded: async function(engineInitializer) {
-    loading.textContent = "menyiapkan";
+    // loadText.textContent = "menyiapkan";
     const appRunner = await engineInitializer.initializeEngine();
 
-    loading.textContent = "menjalankan aplikasi web";
+    // loadText.textContent = "menjalankan aplikasi web";
     await appRunner.runApp();
   }
 });
