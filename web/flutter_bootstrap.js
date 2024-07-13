a89000f163cb046739adc387ef11ec72609ab608
faiq <faiq@voltras.co.id>
2024-07-13 09:05:59 +0700

    up

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,17 +2,22 @@
 {{flutter_build_config}}
 
 const loading = document.createElement('div');
+loading.style.width = '100vw';
+loading.style.height = '100vw';
+loading.style.alignContent = "center";
+loading.style.alignItems = "center";
 const text = document.createElement('p');
 document.body.appendChild(loading);
 loading.appendChild(text);
 
-text.textContent = "mengunduh konten";
+loading.textContent = "mengunduh konten";
+// loading.setAttribute("align", "center");
 _flutter.loader.load({
   onEntrypointLoaded: async function(engineInitializer) {
-    text.textContent = "menyiapkan";
+    loading.textContent = "menyiapkan";
     const appRunner = await engineInitializer.initializeEngine();
 
-    text.textContent = "menjalankan aplikasi web";
+    loading.textContent = "menjalankan aplikasi web";
     await appRunner.runApp();
   }
 });
