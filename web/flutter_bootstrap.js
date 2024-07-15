09f20656255773bdde2c7f98df9f05e344c315bd
faiq <faiq@voltras.co.id>
2024-07-15 08:52:27 +0700

    splash

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,28 +1,28 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const loadingContainer = document.createElement('div');
-loadingContainer.innerHTML = `
-  <div class="bodyload">
-    <div id="splash">
-      <div class="loader"></div>
-    </div>
-    <div id="splashtext" style="color: #3BAF29;">
-      <h3 id="loadText">mengunduh konten</h3>
-    </div>
-  </div>
-`;
+// const loadingContainer = document.createElement('div');
+// loadingContainer.innerHTML = `
+//   <div class="bodyload">
+//     <div id="splash">
+//       <div class="loader"></div>
+//     </div>
+//     <div id="splashtext" style="color: #3BAF29;">
+//       <h3 id="loadText">mengunduh konten</h3>
+//     </div>
+//   </div>
+// `;
 
-document.body.appendChild(loadingContainer);
-let loadText = document.getElementById("loadText");
-loadText.textContent = "mengunduh konten";
+// document.body.appendChild(loadingContainer);
+// let loadText = document.getElementById("loadText");
+// loadText.textContent = "mengunduh konten";
 
 _flutter.loader.load({
   onEntrypointLoaded: async function(engineInitializer) {
-    loadText.textContent = "menyiapkan";
+    // loadText.textContent = "menyiapkan";
     const appRunner = await engineInitializer.initializeEngine();
 
-    loadText.textContent = "menjalankan aplikasi web";
+    // loadText.textContent = "menjalankan aplikasi web";
     await appRunner.runApp();
   }
 });
