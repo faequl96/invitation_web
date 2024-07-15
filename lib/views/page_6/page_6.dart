9d1c00bc4e7562f11d04ac64a69ce1817960a465
faiq <faiq@voltras.co.id>
2024-07-15 22:24:00 +0700

    push

--- lib/views/page_6/page_6.dart
+++ lib/views/page_6/page_6.dart
@@ -208,7 +208,7 @@ class _RSVPItemState extends State<RSVPItem> {
                       )
                     else
                       CardLoading(
-                        height: s(widget.vM.w, 11.6, 12, 12.6, 13.4),
+                        height: s(widget.vM.w, 11.2, 12, 13, 14.2),
                         width: 50 + Random().nextInt(20).toDouble(),
                         borderRadius: const BorderRadius.all(
                           Radius.circular(10),
@@ -248,7 +248,7 @@ class _RSVPItemState extends State<RSVPItem> {
                 else ...[
                   const SizedBox(height: 4),
                   CardLoading(
-                    height: s(widget.vM.w, 8.6, 9, 9.6, 10.4),
+                    height: s(widget.vM.w, 8.2, 9, 10, 11.2),
                     width: 100 + Random().nextInt(60).toDouble(),
                     borderRadius: const BorderRadius.all(Radius.circular(10)),
                   )
@@ -309,13 +309,13 @@ class RSVPSkeleton extends StatelessWidget {
                 Row(
                   children: [
                     CardLoading(
-                      height: s(vM.w, 11.6, 12, 12.6, 13.4),
+                      height: s(vM.w, 11.2, 12, 13, 14.2),
                       width: 50 + Random().nextInt(20).toDouble(),
                       borderRadius: const BorderRadius.all(Radius.circular(10)),
                     ),
                     const SizedBox(width: 8),
                     CardLoading(
-                      height: s(vM.w, 11.6, 12, 12.6, 13.4),
+                      height: s(vM.w, 11.2, 12, 13, 14.2),
                       width: 32,
                       borderRadius: const BorderRadius.all(Radius.circular(10)),
                     ),
@@ -323,19 +323,19 @@ class RSVPSkeleton extends StatelessWidget {
                 ),
                 const SizedBox(height: 5),
                 CardLoading(
-                  height: s(vM.w, 8.6, 9, 9.6, 10.4),
+                  height: s(vM.w, 8.2, 9, 10, 11.2),
                   width: 100 + Random().nextInt(60).toDouble(),
                   borderRadius: const BorderRadius.all(Radius.circular(10)),
                 ),
                 const SizedBox(height: 7),
                 CardLoading(
-                  height: s(vM.w, 9.6, 10, 10.6, 11.4),
+                  height: s(vM.w, 9.2, 10, 11, 12.2),
                   width: 40 + Random().nextInt(20).toDouble(),
                   borderRadius: const BorderRadius.all(Radius.circular(10)),
                 ),
                 const SizedBox(height: 7),
                 CardLoading(
-                  height: s(vM.w, 11.6, 12, 12.6, 13.4),
+                  height: s(vM.w, 11.2, 12, 13, 14.2),
                   width: 140 + Random().nextInt(120).toDouble(),
                   borderRadius: const BorderRadius.all(Radius.circular(10)),
                 ),
